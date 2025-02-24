8f4a39a2e7c25c404fc1b7b8ae857f94689c91d5
Utkarsh Shukla <utkarsh@github.com>
2025-02-24 14:35:44 +0530

    minor edit

--- registry_client.go
+++ registry_client.go
@@ -16,16 +16,16 @@ const (
 )
 
 type Creds struct {
-	auths				map[string]map[string]string
+	auths				map[string]map[string]string `json:"auths"`
 }
 
-type DockerLabels struct {
-	docker				Labels `json:"docker"`
-}
+// type DockerLabels struct {
+// 	docker				Labels `json:"docker"`
+// }
 
-type Labels struct {
-	labels				ArtifactData `json:"labels"`
-}
+// type Labels struct {
+// 	labels				ArtifactData `json:"labels"`
+// }
 
 type ArtifactData struct {
 	jetId				string `json:"jet-id"`
@@ -33,7 +33,11 @@ type ArtifactData struct {
 	projectName 		string `json:"project_key"`
 }
 
-
+type FileInfo struct {
+	created				string `json:"created"`
+	checksums			map[string]string `json:"checksums"`
+	downloadUri			string `json:"downloadUri"`
+}
 
 func init() {
 	//TODO: This may have to be changed in the future
@@ -47,41 +51,46 @@ func NewHTTPClient(clientTimeout int) *http.Client {
 }
 
 func getImageMetaDatas(images []string) ([]ImageMetaData, error) {
-	imageMetaDatas := []ImageMetaData{}
-	for _, image := range images {
-		imageMetaData, err := getImageMetaDataFromImageName(image)
-		if err != nil {
-			return nil, err
-		}
-		imageMetaDatas = append(imageMetaDatas, imageMetaData)
-	}
-	return imageMetaDatas, nil
-}
 
-
-func getImageMetaDataFromImageName(image string) (ImageMetaData, error) {
 	credsFromEnv := os.Getenv("REGISTRY_CREDS")
 	regUrl := ""
 	regToken := ""
 	if credsFromEnv == "" {
-		return ImageMetaData{}, fmt.Errorf("docker registry creds secret not found!")
+		return []ImageMetaData{}, fmt.Errorf("docker registry creds secret not found!")
 	}
 
 	creds :=  Creds{}
 	err := json.Unmarshal([]byte(credsFromEnv), &creds)
 	if err != nil {
-		return ImageMetaData{}, err
+		return []ImageMetaData{}, err
 	}
 	for key, value := range creds.auths {
 		regUrl = key
 		regToken = value["auth"]
 	}
 
+	imageMetaDatas := []ImageMetaData{}
+	for _, image := range images {
+		imageMetaData := ImageMetaData{}
+		if err := getJFrogFileInfo(&imageMetaData, regUrl, image, regToken); err != nil {
+			return []ImageMetaData{}, err
+		}
+		if err2 := getItemProperties(&imageMetaData, regUrl, image, regToken); err2 != nil {
+			return []ImageMetaData{}, err2
+		}
+		imageMetaDatas = append(imageMetaDatas, imageMetaData)
+	}
+	return imageMetaDatas, nil
+}
+
+func getJFrogFileInfo(imageMetaData *ImageMetaData, regUrl string, image string, regToken string) error {
+
 	imageName, imageTag := splitImageName(image)
-	regUrl=regUrl+"/artifactory"+"/"+imageName+"/"+imageTag+"/manifest.json"
-	request, err := http.NewRequest(http.MethodGet, regUrl, nil)
+	endpoint := regUrl+"/api/storage/"+imageName+"/"+imageTag
+
+	request, err := http.NewRequest(http.MethodGet, endpoint, nil)
 	if err != nil {
-		return ImageMetaData{}, err
+		return err
 	}
 
 	request.Header.Add("Content-Type", "application/json")
@@ -89,32 +98,125 @@ func getImageMetaDataFromImageName(image string) (ImageMetaData, error) {
 
 	resp, err := httpClient.Do(request)
 	if err != nil {
-		return ImageMetaData{}, err
+		return err
 	}
 
 	defer resp.Body.Close()
 	
 	if resp.StatusCode != http.StatusOK {
-		return ImageMetaData{}, fmt.Errorf("request to fetch metadata for the image: %s failed with status code: %s",image, resp.StatusCode)
+		return fmt.Errorf("request to fetch metadata for the image: %s failed with status code: %d",image, resp.StatusCode)
 	}
 
 	content, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return ImageMetaData{}, err
+		return err
+	}
+	fileInfo := FileInfo{}
+	if err := json.Unmarshal(content, &fileInfo); err != nil {
+		return err
+	}
+	imageMetaData.artifactLocation = fileInfo.downloadUri
+	imageMetaData.ArtifactCreateDate = fileInfo.created
+	imageMetaData.ArtifactId = fileInfo.checksums["sha256"]
+
+	return nil
+}
+
+func getItemProperties(imageMetaData *ImageMetaData, regUrl string, image string, regToken string) error {
+	
+	imageName, imageTag := splitImageName(image)
+	endpoint := regUrl+"/api/storage/"+imageName+"/"+imageTag+"/manifest.json"
+
+	request, err := http.NewRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return err
 	}
 
-	data := DockerLabels{}
-	if err := json.Unmarshal(content, &data); err != nil {
-		return ImageMetaData{}, nil
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Authorization", "Bearer "+regToken)
+
+	resp, err := httpClient.Do(request)
+	if err != nil {
+		return err
 	}
-	result := ImageMetaData{
-		ArtifactId					:	"???",
-		ArtifactCreateDate			:	"???",
-		JetId						:	data.docker.labels.jetId,
-		SealId						:	data.docker.labels.sealId,
-		DeploymentId				:	"???",
-		ProjectName					:	data.docker.labels.projectName,
-		artifactLocation			:	"artifactory/"+imageName,
+
+	defer resp.Body.Close()
+	
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("request to fetch metadata for the image: %s failed with status code: %d",image, resp.StatusCode)
 	}
-	return result, nil
+
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	itemProperties := map[string]string{}
+	if err := json.Unmarshal(content, &itemProperties); err != nil {
+		return err
+	}
+	imageMetaData.JetId = itemProperties["jet-id"]
+	imageMetaData.SealId = itemProperties["seal_Id"]
+	imageMetaData.ProjectName = itemProperties["project_key"]
+
+	return nil
 }
+
+// func getImageMetaDataFromImageName(image string) (ImageMetaData, error) {
+// 	credsFromEnv := os.Getenv("REGISTRY_CREDS")
+// 	regUrl := ""
+// 	regToken := ""
+// 	if credsFromEnv == "" {
+// 		return ImageMetaData{}, fmt.Errorf("docker registry creds secret not found!")
+// 	}
+
+// 	creds :=  Creds{}
+// 	err := json.Unmarshal([]byte(credsFromEnv), &creds)
+// 	if err != nil {
+// 		return ImageMetaData{}, err
+// 	}
+// 	for key, value := range creds.auths {
+// 		regUrl = key
+// 		regToken = value["auth"]
+// 	}
+
+// 	imageName, imageTag := splitImageName(image)
+// 	regUrl=regUrl+"/artifactory"+"/"+imageName+"/"+imageTag+"/manifest.json"
+// 	request, err := http.NewRequest(http.MethodGet, regUrl, nil)
+// 	if err != nil {
+// 		return ImageMetaData{}, err
+// 	}
+
+// 	request.Header.Add("Content-Type", "application/json")
+// 	request.Header.Add("Authorization", "Bearer "+regToken)
+
+// 	resp, err := httpClient.Do(request)
+// 	if err != nil {
+// 		return ImageMetaData{}, err
+// 	}
+
+// 	defer resp.Body.Close()
+	
+// 	if resp.StatusCode != http.StatusOK {
+// 		return ImageMetaData{}, fmt.Errorf("request to fetch metadata for the image: %s failed with status code: %d",image, resp.StatusCode)
+// 	}
+
+// 	content, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return ImageMetaData{}, err
+// 	}
+
+// 	data := DockerLabels{}
+// 	if err := json.Unmarshal(content, &data); err != nil {
+// 		return ImageMetaData{}, nil
+// 	}
+// 	result := ImageMetaData{
+// 		ArtifactId					:	"???",
+// 		ArtifactCreateDate			:	"???",
+// 		JetId						:	data.docker.labels.jetId,
+// 		SealId						:	data.docker.labels.sealId,
+// 		DeploymentId				:	"???",
+// 		ProjectName					:	data.docker.labels.projectName,
+// 		artifactLocation			:	"artifactory/"+imageName,
+// 	}
+// 	return result, nil
+// }
